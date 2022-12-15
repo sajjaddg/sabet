b2a0f4ee652878befc269ebe12b9d4d953413e4e
sajjaddg <sajjaddg@gmail.com>
2022-12-15 11:20:26 +0330

    add ghPages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {RouterProvider} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import { HashRouter } from 'react-router-dom';
 import router from './routes/router';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <RouterProvider router={router} />
+    </HashRouter>
   </React.StrictMode>
 );
 
